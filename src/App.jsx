c900e2b15aa6cb10b3bc94ee0e3555cdd94dcc0a
Hasan Yildirim <endplay_xp@hotmail.com>
2024-06-23 16:06:04 +0200

    nospline

--- src/App.jsx
+++ src/App.jsx
@@ -6,25 +6,41 @@ import NavBar from "./components/navbar/NavBar";
 import Parallax from "./components/parallax/Parallax";
 import Portfolio from "./components/portfolio/Portfolio";
 import Services from "./components/services/Services";
+import Splin from "./components/spline/Spline";
 // import Test from "./Test";  //testing framer-motion
 
 const App = () => {
-  return <div>
-    <Cursor />
-    {/* <Test /> testframermotion*/}
+	return (
+		<div>
+			<Cursor />
+			{/* <Test /> testframermotion*/}
 
-    <section id="Homepage">
-      <NavBar />
-      <Hero />
-    </section>
-    <section id="Services"><Parallax type="services" /></section>
-    <section id=""><Services /></section>
-    <section id="Portfolio"><Parallax type="portfolio" /></section>
-    <section id=""><Portfolio /></section>
-    <section id=""></section>
-    <section id=""></section>
-    <section id="Contact"><Contact /></section>
-  </div>
+			<section id="Homepage">
+				<NavBar />
+				<Hero />
+			</section>
+			<section id="Services">
+				<Parallax type="services" />
+			</section>
+			<section id="">
+				<Services />
+			</section>
+			<section id="Portfolio">
+				<Parallax type="portfolio" />
+			</section>
+			<section id="">
+				<Portfolio />
+			</section>
+			<section id=""></section>
+			<section id=""></section>
+			<section id="Contact">
+				<Contact />
+			</section>
+			<section>
+				{/* <Splin /> */}
+			</section>
+		</div>
+	);
 };
 
 export default App;
---
+++ src/components/spline/Spline.jsx
@@ -0,0 +1,33 @@
+// import Spline from "@splinetool/react-spline";
+// import { Application } from "@splinetool/runtime";
+import "./spline.scss";
+
+// const canvas = document.getElementById("canvas3d");
+// const app = new Application(canvas);
+// app.load("https://prod.spline.design/GRpWpxMCrBB1HItT/scene.splinecode");
+
+// import { useEffect, useState } from "react";
+
+// const splineResourceUrl = "https://prod.spline.design/GRpWpxMCrBB1HItT/scene.splinecode";
+
+// const Splin = () => {
+// 	const [splineScene, setSplineScene] = useState(null);
+
+// 	useEffect(() => {
+// 		fetch(`https://cors-anywhere.herokuapp.com/${splineResourceUrl}`)
+// 			.then((response) => response.text())
+// 			.then((data) => {
+// 				setSplineScene(data);
+// 			})
+// 			.catch((error) => console.error(error));
+// 	}, [splineResourceUrl]);
+
+// 	return <main>{splineScene && <Spline className="spline" scene={splineScene} />}</main>;
+// };
+
+// <!-- <script type="module" src="https://unpkg.com/@splinetool/viewer@1.6.8/build/spline-viewer.js"></script>;
+// <spline-viewer id="canvas3d" url="https://prod.spline.design/GRpWpxMCrBB1HItT/scene.splinecode"></spline-viewer>; -->
+const Splin = () => {};
+
+//doesnt work
+export default Splin;
